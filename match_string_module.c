0596504f379f41c03d1f20714f8a2ac34d6d83c2
evgeniikozhanov <evgenii.kozhanov@gmail.com>
2021-04-15 13:17:08 +0300

    added module skeleton

---
+++ match_string_module.c
@@ -0,0 +1,51 @@
+#include <Python.h>
+
+typedef struct {
+   char**   result;
+   int      result_length;
+} matchStringResult;
+
+matchStringResult * match_string(const char * sample_str, const char * text_str) {
+    matchStringResult * msr = malloc(sizeof(matchStringResult));
+    msr->result_length = 0;
+    msr->result = malloc(sizeof(msr->result) * 1);
+    msr->result[0] = "test string";
+    return msr;
+}
+
+static PyObject * method_match_string(PyObject *self, PyObject *args) {
+    char *sample_str, *text_str = NULL;
+
+    if(!PyArg_ParseTuple(args, "ss", &sample_str, &text_str)) {
+        return NULL;
+    }
+
+    matchStringResult * result = match_string(sample_str, text_str);
+
+    PyObject *t;
+    t = PyTuple_New(result->result_length);
+    for (int i=0; i<result->result_length; i++) {
+        PyTuple_SetItem(t, i, PyUnicode_FromString(result->result[i]));
+    }
+    free(result->result);
+    free(result);
+
+    return t;
+}
+
+static PyMethodDef MatchStringMethods[] = {
+    {"match_string", method_match_string, METH_VARARGS, "Python interface for match_string C library function"},
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef match_string_module = {
+    PyModuleDef_HEAD_INIT,
+    "match_string",
+    "Python interface for the match_string C library function",
+    -1,
+    MatchStringMethods
+};
+
+PyMODINIT_FUNC PyInit_match_string(void) {
+    return PyModule_Create(&match_string_module);
+}
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup, Extension
+
+
+def main():
+    setup(name="knuth_morris_pratt_algorithm",
+          version="0.0.1",
+          description="Python interface for the Knuth-Morris-Pratt algorithm C library function",
+          author="Evgenii Kozhanov",
+          author_email="evgenii.kozhanov@gmail.com",
+          ext_modules=[Extension("match_string", ["match_string_module.c"])])
+
+
+if __name__ == "__main__":
+    main()
