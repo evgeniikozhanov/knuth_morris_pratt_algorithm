39bf27f41411cf7e496ad14b8925686ae713d99e
evgeniikozhanov <evgenii.kozhanov@gmail.com>
2021-04-18 23:24:45 +0300

    prototype of KMP algorithm

--- match_string_module.c
+++ match_string_module.c
@@ -1,15 +1,51 @@
 #include <Python.h>
 
 typedef struct {
-   char**   result;
+   int*     result;
    int      result_length;
 } matchStringResult;
 
+int * compute_prefix_function(const char * s) {
+    int m = sizeof(s);
+    int * pi = malloc(sizeof(int) * m);
+    pi[0] = 0;
+    int k = -1;
+    for (int i = 1; i < m - 1; i++) {
+        k = pi[i - 1];
+        while ((k > 0) && (s[i] != s[k])) {
+             k = pi[k - 1];
+        }
+        if (s[i] == s[k]) {
+            ++k;
+        }
+        pi[i] = k;
+    }
+    return pi;
+}
+
 matchStringResult * match_string(const char * sample_str, const char * text_str) {
     matchStringResult * msr = malloc(sizeof(matchStringResult));
-    msr->result_length = 1;
-    msr->result = malloc(sizeof(msr->result) * 1);
-    msr->result[0] = "test string";
+    msr->result_length = 0;
+    msr->result = malloc(sizeof(int));
+    int n = sizeof(text_str);
+    int m = sizeof(sample_str);
+    int * pi = compute_prefix_function(sample_str);
+    int q = 0;
+    for (int i = 0; i < n; i++) {
+        while ((q > 0) && (sample_str[q + 1] != text_str[i])) {
+            q = pi[q];
+        }
+        if (sample_str[q + 1] == text_str[i]) {
+            ++q;
+        }
+        if (q == m) {
+            msr->result_length += 1;
+            msr->result = realloc(msr->result, sizeof(int) * msr->result_length);
+            msr->result[msr->result_length - 1] = i - m + 1;
+            q = pi[q];
+        }
+    }
+    free(pi);
     return msr;
 }
 
@@ -25,7 +61,7 @@ static PyObject * method_match_string(PyObject *self, PyObject *args) {
     PyObject *t;
     t = PyTuple_New(result->result_length);
     for (int i=0; i<result->result_length; i++) {
-        PyTuple_SetItem(t, i, PyUnicode_FromString(result->result[i]));
+        PyTuple_SetItem(t, i, PyLong_FromLong(result->result[i]));
     }
     free(result->result);
     free(result);
